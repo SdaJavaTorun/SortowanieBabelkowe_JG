a89141fc418db28d473ce5e9d864ae3b16976dc1
KubaGut <jakub_gutkowski@wp.pl>
2017-03-09 19:14:04 +0100

    version 1.1

--- src/com/sdajava/Sortowanie/Main.java
+++ src/com/sdajava/Sortowanie/Main.java
@@ -3,16 +3,22 @@ package com.sdajava.Sortowanie;
 public class Main {
 
     public static void main(String[] args) {
-	    int [] tab = {1,7,2,3,9,4};
-        int a;
-        for (int i=0; i <tab.length-1; i++){
+	    int [] tab = {1,4,2,3,9,8};
+        int a; boolean flag = true;
+        while (flag){
+            int suma = 0;
             for (int j = 0; j < tab.length-1; j++){
-                if(tab[j] > tab[j+1]){
+                if(tab[j] > tab[j+1]) {
                     a = tab[j];
-                    tab[j] = tab[j+1];
-                    tab[j+1] = a;
+                    tab[j] = tab[j + 1];
+                    tab[j + 1] = a;
+                    suma++;
                 }
             }
+            if (suma == 0){
+                flag = false;
+            }
+
         }
         for (int i=0; i <tab.length; i++)
         System.out.print(tab[i]);
